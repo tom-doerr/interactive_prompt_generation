1f2865a7602bf80fe94148213b69ca28656df8bf
Tom Dörr <tomdoerr96@gmail.com>
2024-06-08 01:41:53 +0200

    Update file

--- main.py
+++ main.py
@@ -3,11 +3,7 @@
 import dspy
 
 lm = dspy.HFClientVLLM(model="microsoft/Phi-3-medium-128k-instruct", port=38242, url="http://localhost", max_tokens=200)
-dspy.settings.configure(lm=lm, trace=[], temperature=0.7)
-
-
-# completion = lm('Hello, my name is', stop='.')
-# print("completion:", completion)
+dspy.settings.configure(lm=lm, trace=[], temperature=1.5)
 
 def build_complete_prompt(json_object_strings, current_prompt):
     complete_prompt = ''
@@ -16,8 +12,7 @@ def build_complete_prompt(json_object_strings, current_prompt):
     complete_prompt += current_prompt
     return complete_prompt
 
-
-task_description = "Generate a banger tweet"
+task_description = "Generate a banger tweet, look at previous tweets and generate a tweet that is appropriate to the feedback and the score given."
 
 json_object_strings = []
 while True:
@@ -28,7 +23,6 @@ while True:
     "task": """{task_description}""",
     "reasoning": """'''
 
-    # completion_reasoning = lm(prompt_reasoning, stop='"""')[0]
     complete_prompt = build_complete_prompt(json_object_strings, prompt_reasoning)
     completion_reasoning = lm(complete_prompt, stop='"""')[0]
     prompt_tweet = f'''
@@ -38,7 +32,6 @@ while True:
     "task": """{task_description}""",
     "reasoning": """{completion_reasoning}"""
     "tweet": """'''
-    # completion_tweet = lm(prompt_tweet, stop='"""')[0]
     complete_prompt = build_complete_prompt(json_object_strings, prompt_tweet)
     completion_tweet = lm(complete_prompt, stop='"""')[0]
     print("complete_prompt:", complete_prompt)
@@ -49,11 +42,11 @@ while True:
     feedback_score = input('Rate the completion from 1 to 5: ')
     complete_json_object = f'''
 {{
-    "feedback": """{feedback_text}""",
-    "score": """{feedback_score}""",
     "task": """{task_description}""",
     "reasoning": """{completion_reasoning}""",
-    "tweet": """{completion_tweet}"""
+    "tweet": """{completion_tweet}""",
+    "feedback": """{feedback_text}""",
+    "score": """{feedback_score}""",
 }}'''
     json_object_strings.append(complete_json_object)
 
